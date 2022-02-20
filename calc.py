8c2aed4cb54b2f1f7d32b2375db4afd9364c1fb1
Houzhong <houzhongy@gmail.com>
2022-02-20 19:55:08 +0800

    Added file to test the unit test on

---
+++ calc.py
@@ -0,0 +1,15 @@
+def add(x, y):
+    # add function
+    return x + y
+
+def subtract(x, y):
+    # subtract function
+    return x - y
+
+def multiply(x, y):
+    # multiply function
+    return x * y
+
+def divide(x, y):
+    # divide function
+    return x / y
